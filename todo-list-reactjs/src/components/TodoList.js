e3e16b523da8f3607aba3b8bcb0c0d5166dc4e22
Douglas <douglas.lisboa@edu.unipar.br>
2020-10-01 00:34:47 -0300

    Adicionado funções de removeção de itens.

--- todo-list-reactjs/src/components/TodoList.js
+++ todo-list-reactjs/src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Todo from './Todo';
 import TodoForm  from './TodoForm';
 
 const TodoList = () => {
@@ -12,13 +13,29 @@ const TodoList = () => {
         const newTodos = [todo, ...todos];
 
         setTodos(newTodos);
-        console.log(...todos);
+    };
+
+    const removeTodo = (id) => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removeArr);
+    };
+
+    const completeTodo = (id) => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
     };
 
     return(
         <div>
             <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
         </div>
     );
 }
